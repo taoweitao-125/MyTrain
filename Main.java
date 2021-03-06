c96cd63c2b6f00b28cce58f6661802307e5717ef
taoweitao-125 <72928481+taoweitao-125@users.noreply.github.com>
2021-03-06 21:37:32 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,80 +1,54 @@
-package day0103;
-
-import java.util.*;
+package day1207;
 
 /**
  * @author xiaogege
  * @描述：
- * @date 2021/1/5
- * @time 14:37
+ * @date 2020/12/8
+ * @time 18:19
  */
-public class Main {
-    public static void func (int n,Map<String,Integer> map) {
-        if (n == 1) {
-            sortMap1(map);
-        }else if (n == 0){
-            sortMap2(map);
-        }
-    }
-
-    public static void sortMap2(Map<String,Integer> inMap){
-        LinkedHashMap<String,Integer> returnMap = new LinkedHashMap<>();
-        //这里将map.entrySet()转换成list
-        List<Map.Entry<String,Integer>> list = new ArrayList<>(inMap.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            //升序排序
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return  (o2.getValue() - o1.getValue());
-            }
-
-        });
-
-        for (Map.Entry<String, Integer> mapping : list) {
-            String key = mapping.getKey();
-            Integer value = mapping.getValue();
-            returnMap.put(key,value);
-        }
-        for (String s : returnMap.keySet()) {
-            System.out.println(s +" " +returnMap.get(s));
-        }
-    }
-    public static void sortMap1(Map<String,Integer> inMap){
-        LinkedHashMap<String,Integer> returnMap = new LinkedHashMap<>();
-        //这里将map.entrySet()转换成list
-        List<Map.Entry<String,Integer>> list = new ArrayList<>(inMap.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            //降序排序
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return  (o1.getValue() - o2.getValue());
-            }
-
-        });
-
-        for (Map.Entry<String, Integer> mapping : list) {
-            String key = mapping.getKey();
-            Integer value = mapping.getValue();
-            returnMap.put(key,value);
-        }
-        for (String s : returnMap.keySet()) {
-            System.out.println(s +" " +returnMap.get(s));
-        }
-    }
-
+import java.util.*;
+public class Main{
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            int m = sc.nextInt();
-            Map<String,Integer> map = new HashMap<>();
-            int n = sc.nextInt();
-            for (int i = 0; i < m; i++) {
-                map.put(sc.next(),sc.nextInt());
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+            String[] st = str.split(" ");
+            int[] arr = new int[10];
+//            b为数字长度
+            int b = 0;
+            for(int i = 0; i < 10 ; i++) {
+                arr[i] = Integer.parseInt(st[i]);
+                b += arr[i];
             }
-            func(n,map);
+            if(b > 50) {
+                System.out.println(-1);
+            }
+            int[] array = new int[b];
+            int c = -1;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < arr[i]; j++) {
+                    array[++c] = i;
+                }
+            }
+
+
+//            将数组中的数按正确顺序排好
+            Arrays.sort(array);
+            int a = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != 0) {
+                    a = i;
+                    break;
+                }
+
+            }
+            array[0] = array[a];
+            array[a] = 0;
+            StringBuilder sb = new StringBuilder();
+            for (int i : array) {
+                sb.append(i);
+            }
+            System.out.println(sb);
         }
-
     }
-
 }
--- Main1.java
+++ Main1.java
@@ -1,34 +1,51 @@
-package day0103;
-
-import java.util.Arrays;
+package day1207;
 
 /**
  * @author xiaogege
  * @描述：
- * @date 2021/1/5
- * @time 14:15
+ * @date 2020/12/8
+ * @time 19:20
  */
+import java.util.Scanner;
 public class Main1 {
-    public static boolean[] chkSubStr(String[] p, int n, String s) {
-        boolean[] ans = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (s.contains(p[i])) {
-                ans[i] = true;
-            }else {
-                 ans[i] = false;
-            }
-        }
-        return ans;
-    }
-
     public static void main(String[] args) {
-        String[] arr = {"ab","b","c","d"};
-        int n = 4;
-        String str = "abc";
-        boolean[] booleans = chkSubStr(arr, 4, str);
-        for (boolean aBoolean : booleans) {
-            System.out.print(aBoolean+" ");
-        }
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+            int b = in.nextInt();
+            if(b == 1) {
+                System.out.println(1);
+            }else {
+                int[] array = new int[b];
+                if(b % 2 == 1) {
+                    int temp = b*b;
+                    for(int i = b/2; i >= 0 ; i--) {
+                        array[i] = temp;
+                        temp -= 2;
+                    }
+                    int tem = b*b + 2;
+                    for(int i = b/2 + 1; i < b ; i++) {
+                        array[i] = tem;
+                        tem += 2;
+                    }
+                }else{
+                    int c = b*b + 1;
+                    for(int i = b/2; i >= 0 ; i--) {
+                        array[i] = c;
+                        c -= 2;
+                    }
+                    int tem1 = b*b + 3;
+                    for(int i = b/2 + 1; i < b ; i++) {
+                        array[i] = tem1;
+                        tem1 += 2;
+                    }
+                }
 
+                for (int i = 0; i < array.length - 1; i++) {
+                    System.out.print(array[i] + "+");
+                }
+                System.out.println(array[array.length - 1]);
+            }
+
+        }
     }
 }
